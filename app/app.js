5d9ab52b57efcc3f67e30079a9118754ee2cff6c
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2016-01-29 10:41:11 +0530

    Angular-Travel Files Added

---
+++ app/app.js
@@ -0,0 +1,35 @@
+/*==========================================================
+    Author      : Ranjithprabhu K
+    Date Created: 24 Dec 2015
+    Description : main application base
+    
+    Change Log
+    s.no      date    author     description     
+    
+
+ ===========================================================*/
+
+var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'flash',
+    //main modules
+    'login']);
+
+
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider, $modalInstance) {
+
+    //IdleScreenList
+    $stateProvider
+       .state('app', {
+           url: '/app',
+           templateUrl: 'app/common/app.html',
+           controller: 'appCtrl',
+           controllerAs: 'vm',
+           data: {
+               pageTitle: 'Login'
+           }
+       });
+
+    $urlRouterProvider.otherwise('app');
+}]);
+
+// set global configuration of application and it can be accessed by injecting appSettings in any modules
+app.constant('appSettings', appConfig);
